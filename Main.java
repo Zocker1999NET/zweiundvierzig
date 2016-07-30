4097b1732065a04a3b3eca571d05e1279ed906f9
Zocker1999NET <zocker.net@live.de>
2016-07-30 13:10:13 +0200

    BlueJ-Integration

---
+++ Main.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class main here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Main
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class main
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Start zweiundvierzig as JavaFX ...");
+    }
+}
